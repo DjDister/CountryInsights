7fc50d19bb1e1fb1e5555254b7fea0b57350b07c
DjFIREED <porebskifilip@wp.pl>
2023-04-22 07:50:27 +0200

    custom map with chosen countries marked

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
 import "./App.css";
+import WorldMap from "./components/WorldMap/WorldMap";
+import { chosenCountryType } from "../types";
 
 function App() {
-  return <>Country Insights</>;
+  const [chosenCountries, setChosenCountries] = useState<chosenCountryType[]>([
+    { name: "Poland", color: "red" },
+    { name: "Germany", color: "blue" },
+  ]);
+
+  return (
+    <>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div style={{ width: "50%" }}>
+          <WorldMap chosenCountries={chosenCountries} />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
