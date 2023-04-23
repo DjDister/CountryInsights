538a5943182ba920ffbfe97933c42524abafc358
DjFIREED <porebskifilip@wp.pl>
2023-04-23 11:09:56 +0200

    sort function

---
+++ src/utils/sortAlphabetically.ts
@@ -0,0 +1,9 @@
+const sortAlphabetically = <T>(array: T[], key: keyof T): T[] => {
+  return array.sort((a, b) => {
+    if (a[key] < b[key]) return -1;
+    if (a[key] > b[key]) return 1;
+    return 0;
+  });
+};
+
+export default sortAlphabetically;
